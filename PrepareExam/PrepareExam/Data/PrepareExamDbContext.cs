ee6b292438b4542c8730019723c828b016a32d7e
muzaffer <muzaffer.deveci@tazebt.com>
2021-12-28 04:11:32 +0300

    Login Register Added

---
+++ PrepareExam/PrepareExam/Data/PrepareExamDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrepareExam.Data
+{
+    public class PrepareExamDbContext : IdentityDbContext
+    {
+        private readonly DbContextOptions _options;
+
+        public PrepareExamDbContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
